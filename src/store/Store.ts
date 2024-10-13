5c58552c6143dd36c7ceb3b0a6565c0a38225136
nikit <snikit086@gmail.com>
2024-10-13 15:00:33 +0530

    firts commit

---
+++ src/store/Store.ts
@@ -0,0 +1,32 @@
+
+export type UserId = string;
+
+export interface Chat{
+    id : string;
+    name : string ;
+    userId : UserId;
+    message : string;
+    upvotes : UserId[];
+}
+
+export abstract class Store {
+    constructor() {
+        
+    }
+
+    initializingRoom(roomId : string)  {
+
+    }
+
+    getChats(room : string , limit : number , offset : number) {
+
+    }
+    
+    addChats(userId : UserId , room : string , name : string , message : string) {
+
+    }
+
+    upVote( userId : UserId , room : string , chatId : string) {
+
+    }
+}
